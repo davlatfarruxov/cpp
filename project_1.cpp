13f6ed31f04c10b8c85a182797d791ac3ccf65ff
Davlat Farruxov <76444252+davlatfarruxov@users.noreply.github.com>
2022-10-11 23:29:17 +0500

    Create project_1.cpp

---
+++ project_1.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+    //Birinchi mas'ala
+
+    /*
+    int a, P;
+    cout << "Kvadratning tomonini kiriting:" << endl << "a="; cin>>a;
+    P=a*4;
+    cout << "Kvadratning peremetri=" << P;
+    */
+
+    //Ikkinchi mas'ala
+
+    /*
+    int a, S;
+    cout << "Kvadratning tomonini kiriting:" << endl << "a="; cin>>a;
+    S=a*a;
+    cout << "Kvadratning yuzasi=" << S;
+    */
+
+    //Uchinchi mas'ala
+
+    /*
+    int a,b,P,S;
+    cout << "To'g'ri to'rburchakning tomonlari a va b ni kiriting" << endl << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    P=(a+b)*2;
+    S=a*b;
+    cout << "To'rtburchakning peremetri=" << P << endl << "To'rtburchakning yuzasi=" << S;
+    */
+
+    //To'rtinchi mas'ala
+
+    /*
+    int d;
+    double L;
+    const double Pi=3.14;
+    cout << "Aylana diametrini kiriting:" << endl << "d="; cin >> d;
+    L=d*Pi;
+    cout << "Aylana uzunligi:" << L;
+    */
+
+    //Beshinchi mas'ala
+
+    /*
+    int a, V, S;
+    cin >> a;
+    V=a*a;
+    S=V*6;
+    cout << "Kubning hajmi:" << V << endl << "Kubning to'la sirti:" << S;
+    */
+
+    //Oltinchi mas'ala
+
+    /*
+    int a,b,c,V,S;
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    cout << "c="; cin >> c;
+    V=a*b*c;
+    S=2*(a*b+b*c+a*c);
+    cout << "Parallipiped hajmi:" << V << endl << "Parallipiped to'la sirti:" << S;
+    */
+
+    //Yettinchi mas'ala
+
+    /*
+    int R;
+    double L,S;
+    const double Pi=3.14;
+    cout << "Doira radiusini kiriting:" << endl << "R="; cin >> R;
+    L=2*R*Pi;
+    S=R*R*Pi;
+    cout << "Doiraning uzunligi:" << L << endl << "Doiraning yuzasi:" << S;
+    */
+
+    //Sakkizinchi mas'ala
+
+    /*
+    int a,b,x;
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    x=(a+b)/2;
+    cout << "Bu sonlarning o'rta arifmetigi:" << x;
+    */
+
+    //To'qqizinchi mas'ala
+
+    /*
+    int a,b;
+    double F;
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    F=sqrt(a*b);
+    cout << "Bu sonlarning o'rta geometrigi:" << F;
+    */
+
+    //O`ninchi mas'ala
+
+    /*
+    int a,b,x,y,z,f;
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    x=a+b;
+    y=a*b;
+    z=a*a;
+    f=b*b;
+    cout << "Bu sonlarning yig'indisi:" << x << endl << "Bu sonlarning ko'paytmasi:" << y << endl << "a ning kvadrati:" << z << endl << "b ning kvadrati:" << f;
+    */
+
+    //O`n birinchi mas'ala
+
+    /*
+    int a,b,x,y,z,f;
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    x=a+b;
+    y=a*b;
+    z=abs(a);
+    f=abs(b);
+    cout << "Bu sonlarning yig'indisi:" << x << endl << "Bu sonlarning ko'paytmasi:" << y << endl << "a ning moduli:" << z << endl << "b ning moduli:" << f;
+    */
+
+    //O'n ikkinchi mas'ala
+
+    /*
+    int a,b;
+    double c,P;
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    c=sqrt((a*a)+(b*b));
+    P=a+b+c;
+    cout << "Uchburchakning gepotenuzasi : " << c << endl << "Uchburchakning Peremetri : " << P;
+    */
+
+    //O'n uchinchi mas'ala
+
+    /*
+    int a,b;
+    double c1,c2,K;
+    const double Pi=3.14;
+    cout << "R1="; cin >> a;
+    cout << "R2="; cin >> b;
+    c1=Pi*a;
+    c2=Pi*b;
+    K=Pi*(c1-c2);
+    cout << "Birinchi aylananing yuzasi: " << c1 << endl << "Ikkinchi aylaning yuzasi: " << c2 << endl << "Aylanalarning ayirmasi: " << K;
+    */
+
+    //O`n to'rtinchi mas'ala
+
+    /*
+    int L;
+    double S,R;
+    const double Pi=3.14;
+    cout << "L="; cin >> L;
+    R=L/2/Pi;
+    S=Pi*(R*R);
+    cout << "Aylana radiusi: " << R << endl << "Aylana yuzasi: " << S;
+    */
+
+    //O'n beshinchi
+
+    /*
+    int S;
+    double d,R2,R;
+    const double Pi=3.14;
+    cout << "S="; cin >> S;
+    R2=S/Pi;
+    R=sqrt(R2);
+    d=R*2;
+    cout << "Aylana radiusi: " << R << endl << "Aylana diametri: " << d;
+    */
+
+return 0;
+}
